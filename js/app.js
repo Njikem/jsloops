dfe5a3b894cddcaee8ac15860b4fe5fb79da01ed
Njikem <123434988+Njikem@users.noreply.github.com>
2023-07-15 16:05:38 -0600

    Loops

---
+++ js/app.js
@@ -0,0 +1,22 @@
+let numb = 0;
+while (numb < 51){
+
+    console.log(numb);
+    numb = numb + 1;
+}
+
+let counter = 0;
+while (counter < -50){
+console.log(counter);
+counter = counter + 1;
+}
+
+
+let numbers = 0;
+
+while(numbers < 100){
+    if(numbers % 2 === 0)
+    console.log(numbers);
+    numbers = numbers + 1;
+}
+
